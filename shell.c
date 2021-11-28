67b735ff89cd2256e72c7b8e63f1ef3058636566
Wojciech Adamiec <wojciechadamiec99@gmail.com>
2021-11-28 21:18:56 +0100

    Try sleep

--- shell.c
+++ shell.c
@@ -105,6 +105,7 @@ static int do_job(token_t *token, int ntokens, bool bg) {
       msg("[%d] running '%s'\n", new_job, jobcmd(new_job));
     }
   } else { // Child
+    sleep(1);
     setpgid(0, 0);
 
     Sigprocmask(SIG_SETMASK, &mask, NULL);
